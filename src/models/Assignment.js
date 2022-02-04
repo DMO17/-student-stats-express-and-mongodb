243038d0fdcbdabc697b2e544a24e13e3d161188
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-04 16:18:05 +0000

    assigment schema done

--- src/models/Assignment.js
+++ src/models/Assignment.js
@@ -1,5 +1,28 @@
-const { Schema, model } = require("mongoose");
+const { Schema } = require("mongoose");
+const moment = require("moment");
 
-const assignmentSchema = new Schema({});
+const assignmentSchema = new Schema({
+  assignmentId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    auto: true,
+  },
+  assignmentName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+  },
+  score: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100,
+  },
+  createdAt: {
+    type: Date,
+    default: moment(),
+  },
+});
 
 module.exports = assignmentSchema;
