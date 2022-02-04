a24e820f7ec4b0a50cf223a76654c307169d5bca
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-04 15:59:25 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const mongoose = require("mongoose");
+// const express = require("express");
+// const mongoose = require("mongoose");
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(routes);
 
-const init = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/studentStatsDb", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+// const init = async () => {
+//   try {
+//     await mongoose.connect("mongodb://localhost:27017/studentStatsDb", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
 
-    console.log("[INFO]: Successfully Connected to the database");
+//     console.log("[INFO]: Successfully Connected to the database");
 
-    app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
-    );
-  } catch (error) {
-    console.log(
-      `[Error]: Connection to the Database has failed | ${error.message}`
-    );
-  }
-};
+//     app.listen(PORT, () =>
+//       console.log(`Server running on http://localhost:${PORT}`)
+//     );
+//   } catch (error) {
+//     console.log(
+//       `[Error]: Connection to the Database has failed | ${error.message}`
+//     );
+//   }
+// };
 
-init();
+// init();
--- src/models/Student.js
+++ src/models/Student.js
@@ -1,6 +1,22 @@
 const { Schema, model } = require("mongoose");
 
-const studentSchema = {};
+const studentSchema = {
+  firstName: {
+    type: String,
+    require: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    require: true,
+    maxLength: 50,
+  },
+  githubUrl: {
+    type: String,
+    require: true,
+    maxLength: 50,
+  },
+};
 
 const schema = new Schema(studentSchema);
 const Student = model("student", schema);
