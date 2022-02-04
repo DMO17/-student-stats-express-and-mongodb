28aec75028b92391581cd30770d81ecc02e8335c
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-04 21:56:53 +0000

    seeded cources data in to mongo database

--- src/seeds/data/course.js
+++ src/seeds/data/course.js
@@ -9,6 +9,16 @@ const courses = [
     inPerson: true,
     students: [],
   },
+  {
+    courseName: "MatLab and Simulink",
+    inPerson: true,
+    students: [],
+  },
+  {
+    courseName: "3D CAD DRAWING",
+    inPerson: true,
+    students: [],
+  },
 ];
 
 module.exports = courses;
--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-const { Student } = require("../models");
+const { Student, Course } = require("../models");
+
 const students = require("./data/student");
+const courses = require("./data/course");
 
 const init = async () => {
   try {
@@ -15,6 +17,25 @@ const init = async () => {
 
     await Student.deleteMany({});
     await Student.insertMany(students);
+
+    console.log("[INFO]: Students data has been seeded in the mongoDB");
+
+    // bulk create courses document
+
+    const studentsDataFromDb = await Student.find({});
+
+    studentsDataFromDb.forEach((student) => {
+      const studentId = student._id.toString();
+
+      const randomCourse = courses[Math.floor(Math.random() * courses.length)];
+
+      randomCourse.students.push(studentId);
+    });
+
+    await Course.deleteMany({});
+    await Course.insertMany(courses);
+
+    console.log("[INFO]: Courses data has been seeded in the mongoDB");
 
     await mongoose.disconnect();
   } catch (error) {
