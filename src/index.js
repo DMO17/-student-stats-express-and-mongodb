29a0523b26fbe3c0465bf5166e5e513e32742a01
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-04 15:47:51 +0000

    added and connected to mongodb using mongoose

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const routes = require("./routes");
 
@@ -11,9 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 const init = async () => {
-  app.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${PORT}`)
-  );
+  try {
+    await mongoose.connect("mongodb://localhost:27017/studentStatsDb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("[INFO]: Successfully Connected to the database");
+
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(
+      `[Error]: Connection to the Database has failed | ${error.message}`
+    );
+  }
 };
 
 init();
